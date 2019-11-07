48dc3d1fb0a69d823cc23867f94cffc79254c4b2
TheEmrio <emrio@emrio.fr>
2019-11-07 16:15:24 +0100

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,12 +2,10 @@ import mongoose from 'mongoose'
 
 export interface User extends mongoose.Document {
   username: string
-  // password: string
   rfid: string
 }
 
 export const User = mongoose.model<User>('User', new mongoose.Schema({
   username: String,
-  // password: String,
   rfid: String
 }))
