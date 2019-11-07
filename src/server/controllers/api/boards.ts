1b62fe82fcd849f76604832616b32f37d8242c54
TheEmrio <emrio@emrio.fr>
2019-11-07 16:15:09 +0100

    dashboard server side

--- src/server/controllers/api/boards.ts
+++ src/server/controllers/api/boards.ts
@@ -1,11 +1,19 @@
 import { RequestHandler } from 'express'
+import moment from 'moment'
+import { getUsersHistory } from '../../../models/Update'
 
 export const getWelcomePage: RequestHandler = function (_req, res) {
   return res.api.buildBoard('home', 'Bouquineur :: Welcome')
 }
 
-export const getDashboard: RequestHandler = function (_req, res) {
-  return res.api.buildBoard('dashboard', 'Bouquineur :: Dashboard')
+export const getDashboard: RequestHandler = function (req, res) {
+  if (!req.user) return res.api.buildError(500, 'User error')
+  getUsersHistory(req.user._id)
+    .then(history => res.api.buildBoard('dashboard', 'Bouquineur :: Dashboard', { user: req.user, history, moment }))
+    .catch(err => {
+      console.error(err)
+      res.api.buildError(500, 'Error while fetching history')
+    })
 }
 
 export const browseLibrary: RequestHandler = function (_req, res) {
