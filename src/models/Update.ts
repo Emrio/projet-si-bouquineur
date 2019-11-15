bc033838b9c866da0105b6f6759f3b784a5ac39b
TheEmrio <emrio@emrio.fr>
2019-11-15 22:51:18 +0100

    Update: added a method and better model definition

--- src/models/Update.ts
+++ src/models/Update.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { User } from './User'
-import { Book } from './Book'
+import { Book, setBookStatus } from './Book'
+
+export type Action = 'WITHDRAW' | 'RETURN'
 
 export interface Update extends mongoose.Document {
   book: Book
-  action: 'WITHDRAW' | 'RETURN'
+  action: Action
   user: User
   date: Date
 }
@@ -13,10 +15,19 @@ export const Update = mongoose.model<Update>('Update', new mongoose.Schema({
   book: { type: mongoose.Types.ObjectId, ref: 'Book' },
   action: String,
   user: { type: mongoose.Types.ObjectId, ref: 'User' },
-  date: Date
+  date: { type: Date, default: Date.now }
 }))
 
 export async function getUserHistory (id: mongoose.Types.ObjectId): Promise<Update[]> {
   const updates = await Update.find({ user: id })
   return await Promise.all(updates.map(update => update.populate('book').execPopulate()))
+}
+
+export async function updateBookStatus (book: mongoose.Types.ObjectId, user: mongoose.Types.ObjectId, action: Action): Promise<Update> {
+  const update = new Update({ book, user, action })
+  await update.save()
+  console.log(1)
+  await setBookStatus(book, action === 'WITHDRAW' ? user : null, action === 'WITHDRAW' ? new Date() : undefined)
+  console.log(2)
+  return update
 }
