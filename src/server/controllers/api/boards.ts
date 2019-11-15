f1ef9042eed860233d0eb77751065aafa03c8255
TheEmrio <emrio@emrio.fr>
2019-11-15 22:53:04 +0100

    browse library + borrowed books

--- src/server/controllers/api/boards.ts
+++ src/server/controllers/api/boards.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express'
 import moment from 'moment'
 import { getUserHistory } from '../../../models/Update'
+import { getLibrary, getBorrowed, getBook } from '../../../models/Book'
 
 export const getWelcomePage: RequestHandler = function (_req, res) {
   return res.api.buildBoard('home', 'Bouquineur :: Welcome')
@@ -9,7 +10,7 @@ export const getWelcomePage: RequestHandler = function (_req, res) {
 export const getDashboard: RequestHandler = function (req, res) {
   if (!req.user) return res.api.buildError(500, 'User error')
   getUserHistory(req.user._id)
-    .then(history => res.api.buildBoard('dashboard', 'Bouquineur :: Dashboard', { user: req.user, history, moment }))
+    .then(history => res.api.buildBoard('dashboard', 'Bouquineur :: Tableau de bord', { user: req.user, history, moment }))
     .catch(err => {
       console.error(err)
       res.api.buildError(500, 'Error while fetching history')
@@ -17,9 +18,22 @@ export const getDashboard: RequestHandler = function (req, res) {
 }
 
 export const browseLibrary: RequestHandler = function (_req, res) {
-  return res.api.buildBoard('browse', 'Bouquineur :: Browse Library')
+  getLibrary()
+    .then(library => res.api.buildBoard('browse', 'Bouquineur :: Bibliothèque', { library, moment }))
+    .catch(err => {
+      console.error(err)
+      res.api.buildError(500, 'Error while fetching history')
+    })
 }
 
-export const myBooks: RequestHandler = function (_req, res) {
-  return res.api.buildBoard('borrowed', 'Bouquineur :: Borrowed Books')
+}
+
+export const myBooks: RequestHandler = function (req, res) {
+  if (!req.user) return res.api.buildError(500, 'User error')
+  getBorrowed(req.user._id)
+    .then(borrowed => res.api.buildBoard('borrowed', 'Bouquineur :: Borrowed Books', { borrowed, moment }))
+    .catch(err => {
+      console.error(err)
+      res.api.buildError(500, 'Error while fetching history')
+    })
 }
